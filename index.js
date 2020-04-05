b9801d4995376c682fbc396281e2f0b32e990adb
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2020-04-05 19:06:51 +0200

    update to v0.2.0, add cli

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+'use strict'
+
 const fs = require('fs')
 const chromeWebstoreUpload = require('chrome-webstore-upload')
 
@@ -18,8 +21,14 @@ function getRefreshToken (parameters) {
   })
 }
 
+function handleVars (input) {
+  const version = process.env.npm_package_version
+  const home = process.env.HOME
+  return input.replace('{version}', version).replace('{home}', home)
+}
+
 async function updateAndPublish (parameters) {
-  const credentials = parameters.credentials || JSON.parse(fs.readFileSync(parameters.credentialsPath))
+  const credentials = parameters.credentials || JSON.parse(fs.readFileSync(handleVars(parameters.credentialsPath)))
   const refreshToken = credentials.refreshToken || await getRefreshToken()
 
   const webStoreApi = chromeWebstoreUpload({
@@ -31,7 +40,7 @@ async function updateAndPublish (parameters) {
 
   const accessToken = await webStoreApi.fetchToken()
 
-  const packageStream = parameters.package || fs.createReadStream(parameters.packagePath)
+  const packageStream = parameters.package || fs.createReadStream(handleVars(parameters.packagePath))
   const uploadResult = await webStoreApi.uploadExisting(packageStream, accessToken)
   console.log(uploadResult)
 
@@ -43,6 +52,21 @@ const updatePackage = parameters => {
   return updateAndPublish(parameters)
     .catch(console.error)
     .then(process.exit)
+}
+
+const argList = process.argv.join('=').split('=')
+if (argList.length > 2) {
+  let params = {}
+  argList.forEach((item, index) => {
+    if (item === '--credentials-path' || item === '-cp') {
+      params.credentialsPath = argList[index + 1]
+    } else if (item === '--package-path' || item === '-pp') {
+      params.packagePath = argList[index + 1]
+    } else if (item === '--extension-id' || item === '-e') {
+      params.extensionId = argList[index + 1]
+    }
+  })
+  updatePackage(params)
 }
 
 exports.publish = updatePackage
