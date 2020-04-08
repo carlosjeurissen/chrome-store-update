ac821c1dc310c1eabcab7049e60e34eae32fb123
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2020-04-08 09:00:59 +0200

    update to v0.2.1, codestyle updates

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
 #!/usr/bin/env node
-'use strict'
+'use strict';
 
-const fs = require('fs')
-const chromeWebstoreUpload = require('chrome-webstore-upload')
+const fs = require('fs');
+const chromeWebstoreUpload = require('chrome-webstore-upload');
 
 function getRefreshToken (parameters) {
-  const standardInput = process.stdin
+  const standardInput = process.stdin;
 
-  standardInput.setEncoding('utf-8')
-  standardInput.setRawMode(true)
+  standardInput.setEncoding('utf-8');
+  standardInput.setRawMode(true);
 
-  console.log('Enter your refresh token')
+  console.log('Enter your refresh token');
 
   return new Promise(function (resolve) {
     standardInput.on('data', function (data) {
-      standardInput.setRawMode(false)
-      console.log('Thanks! Uploading now...')
-      resolve(data)
-    })
-  })
+      standardInput.setRawMode(false);
+      console.log('Thanks! Uploading now...');
+      resolve(data);
+    });
+  });
 }
 
 function handleVars (input) {
-  const version = process.env.npm_package_version
-  const home = process.env.HOME
-  return input.replace('{version}', version).replace('{home}', home)
+  const version = process.env.npm_package_version;
+  const home = process.env.HOME;
+  return input.replace('{version}', version).replace('{home}', home);
 }
 
 async function updateAndPublish (parameters) {
-  const credentials = parameters.credentials || JSON.parse(fs.readFileSync(handleVars(parameters.credentialsPath)))
-  const refreshToken = credentials.refreshToken || await getRefreshToken()
+  const credentials = parameters.credentials || JSON.parse(fs.readFileSync(handleVars(parameters.credentialsPath)));
+  const refreshToken = credentials.refreshToken || await getRefreshToken();
 
   const webStoreApi = chromeWebstoreUpload({
-    'extensionId': parameters.extensionId,
-    'clientId': credentials.clientId,
-    'clientSecret': credentials.clientSecret,
-    'refreshToken': refreshToken
-  })
+    extensionId: parameters.extensionId,
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    refreshToken: refreshToken
+  });
 
-  const accessToken = await webStoreApi.fetchToken()
+  const accessToken = await webStoreApi.fetchToken();
 
-  const packageStream = parameters.package || fs.createReadStream(handleVars(parameters.packagePath))
-  const uploadResult = await webStoreApi.uploadExisting(packageStream, accessToken)
-  console.log(uploadResult)
+  const packageStream = parameters.package || fs.createReadStream(handleVars(parameters.packagePath));
+  const uploadResult = await webStoreApi.uploadExisting(packageStream, accessToken);
+  console.log(uploadResult);
 
-  const publishResult = await webStoreApi.publish('default', accessToken)
-  console.log(publishResult)
+  const publishResult = await webStoreApi.publish('default', accessToken);
+  console.log(publishResult);
 }
 
 const updatePackage = parameters => {
   return updateAndPublish(parameters)
     .catch(console.error)
-    .then(process.exit)
-}
+    .then(process.exit);
+};
 
-const argList = process.argv.join('=').split('=')
+const argList = process.argv.join('=').split('=');
 if (argList.length > 2) {
-  let params = {}
+  const params = {};
   argList.forEach((item, index) => {
     if (item === '--credentials-path' || item === '-cp') {
-      params.credentialsPath = argList[index + 1]
+      params.credentialsPath = argList[index + 1];
     } else if (item === '--package-path' || item === '-pp') {
-      params.packagePath = argList[index + 1]
+      params.packagePath = argList[index + 1];
     } else if (item === '--extension-id' || item === '-e') {
-      params.extensionId = argList[index + 1]
+      params.extensionId = argList[index + 1];
     }
-  })
-  updatePackage(params)
+  });
+  updatePackage(params);
 }
 
-exports.publish = updatePackage
-module.exports = exports.publish
+exports.publish = updatePackage;
+module.exports = exports.publish;
